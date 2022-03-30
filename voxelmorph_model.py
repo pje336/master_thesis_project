7fd78855860b8b20454ef63074ff33bd9bb1cc71
Thomas van der Meulen <t.vandermeulen-1@student.tudelft.nl>
2022-03-30 17:25:38 +0200

    ajust the training routine to work with the new dataset generator

--- voxelmorph_model.py
+++ voxelmorph_model.py
@@ -1,70 +1,64 @@
 import numpy as np
-from data_importer import import_CT_data
 from slice_viewer import slice_viewer
 import voxelmorph as vxm
 import torch
 from datetime import datetime
+from dataset_generator import dataset_generator
+from ct_path_dict import ct_path_dict
 
+# data parameters
+root_path = "C:/Users/pje33/Google Drive/Sync/TU_Delft/MEP/4D_lung_CT/4D-Lung/"
+patient_id = 107
+scan_id = "06-02-1999-p4-89680"
+m_phases = [0, 30]
+f_phase = 50
+z_max = 80
 
+# Obtain data
+dataset = dataset_generator(patient_id, scan_id, m_phases, f_phase, root_path, ct_path_dict)
+vol_shape = np.array(dataset[:, :, :z_max, :, :].shape)[-3:]  # input shape
 
+# Network parameters for voxelmorph
 
-
-path_drive = "C:/Users/pje33/Google Drive/Sync/TU_Delft/MEP/"
-path_CT_0 = "4D_lung_CT/4D-Lung/107_HM10395/05-26-1999-p4-39328/1.000000-P4P107S300I00003 Gated 0.0A-97958/"
-path_CT_90 = "4D_lung_CT/4D-Lung/107_HM10395/05-26-1999-p4-39328/1.000000-P4P107S300I00012 Gated 90.0A-89848/"
-
-CT_data_0 = import_CT_data(path_drive, path_CT_0)[:80]
-CT_data_90 = import_CT_data(path_drive, path_CT_90)[:80]
-
-
-
-
-# network parameters for voxelmorph
-vol_shape = np.array(np.shape(CT_data_0)) #input shape
 nb_features = [
     [16, 32, 32, 32],
-    [ 32, 32, 32, 16]
-] # number of features of encoder and decoder
+    [32, 32, 32, 16]]  # number of features of encoder and decoder
 
-losses = vxm.losses.Grad('l2').loss
+losses = [vxm.losses.MSE().loss, vxm.losses.Grad('l2').loss]
 loss_weights = [1, 0.01]
 
 vxm_model = vxm.networks.VxmDense(vol_shape, nb_features, int_steps=0)
-
-
 learning_rate = 1e-3
-optimizer = torch.optim.Adam(vxm_model.parameters(), lr=learning_rate)
 
-x_data = torch.tensor(CT_data_0[np.newaxis,np.newaxis,  ...], dtype=torch.float)
-y_data = torch.tensor(CT_data_90[np.newaxis,np.newaxis, ...], dtype=torch.float)
-print(np.shape(x_data))
-# print(vxm_model)
+
 def train(epochs):
+    torch.backends.cudnn.deterministic = True
+    vxm_model.train()
+    optimizer = torch.optim.Adam(vxm_model.parameters(), lr=learning_rate)
+
     for epoch in range(epochs):
-        print("epoch {} of {}".format(epoch+1 ,epochs))
-        vxm_model.train()
-        # for x_data, y_data:
+        print("epoch {} of {}".format(epoch + 1, epochs))
 
-        prediction, pos_flow = vxm_model(x_data,y_data)
-        np.save("predicted_CT_epoch_{}.npy".format(epoch+1),prediction[0,0,...].detach().numpy())
-        print(np.shape(prediction))
+        # iterate over all image pairs in the dataset.
+        for index in range(dataset.shape[0]):
+            fixed_tensor = dataset[None, None, index, 0, ...]
+            moving_tensor = dataset[None, None, index, 1, ...]
 
-        print("calculate losses")
-        loss_function = vxm.losses.MSE()
-        loss = loss_function.loss(y_data,prediction)
-        print("loss:",loss)
-        optimizer.zero_grad()
-        loss.backward()
-        print("done")
-        optimizer.step()
-    torch.save(vxm_model, "./saved_models/voxelmorph_model_timestamp_{}.pth".format(datetime.now().strftime("%Y_%m_%d_%H_%M_%S")))
+            prediction, pos_flow = vxm_model(fixed_tensor, moving_tensor)
+            np.save("predicted_CT_epoch_{}.npy".format(epoch + 1), prediction[0, 0, ...].detach().numpy())
+            print(np.shape(prediction))
+
+            print("calculate losses")
+            loss_function = vxm.losses.MSE()
+            loss = loss_function.loss(moving_tensor, prediction)
+            print("loss:", loss)
+            optimizer.zero_grad()
+            loss.backward()
+            print("done")
+            optimizer.step()
+    torch.save(vxm_model,
+               "./saved_models/voxelmorph_model_timestamp_{}.pth".format(datetime.now().strftime("%Y_%m_%d_%H_%M_%S")))
     print("model saved")
 
+
 train(3)
-
-
-
-
-
-
-
