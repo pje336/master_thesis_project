f24ed9d66b759057d670154449ad2e8f10b9bcb8
Thomas van der Meulen <t.vandermeulen-1@student.tudelft.nl>
2022-03-31 15:11:54 +0200

    Modify dataset_generator to make a dataset class object with f and m image pairs

--- dataset_generator.py
+++ dataset_generator.py
@@ -3,11 +3,34 @@ import os
 import numpy as np
 import pydicom
 import torch
+from torch.utils.data import Dataset
 
 
-def import_CT_data(root_path, filepath):
+class ct_dataset(Dataset):
     """
-    Imports all CT data from dcm files in the given directory and normalises the data.
+    Dataset class with fixed and moving tensor pairs of shape [len(phases),1,z,x,y]
+    """
+
+    def __init__(self, fixed, moving):
+        self.fixed = fixed
+        self.moving = moving
+
+    def __len__(self):
+        return len(self.fixed)
+
+    def __getitem__(self, index):
+        _fixed = self.fixed[index]
+        _moving = self.moving[index]
+
+        return _fixed, _moving
+
+    def shape(self):
+        return self.fixed.shape
+
+
+def read_CT_data_file(root_path, filepath):
+    """
+    Imports all CT data by reading all dcm files in the given directory and normalises the data.
     :param root_path: [string] Root path to 4DCT folders.
     :param filepath: [string] String with the specific folder which holds CT data.
     :return: CT_data: [3d array] 3d numpy array of shape [z,x,y] with normalised CT data.
@@ -50,32 +73,33 @@ def CT_dataset(patient_id, scan_id, phases, root_path, ct_path_dict):
             return
 
         if i == 0:  # make the initial array on the first run.
-            ct_data = np.zeros(((len(phases),) + np.shape(import_CT_data(root_path, ct_filepath))))
+            ct_data = np.zeros(((len(phases),) + np.shape(read_CT_data_file(root_path, ct_filepath))))
 
-        ct_data[i] = import_CT_data(root_path, ct_filepath)
+        ct_data[i] = read_CT_data_file(root_path, ct_filepath)
+
     return ct_data
 
 
-def dataset_generator(patient_id, scan_id, m_phases, f_phase, root_path, ct_path_dict):
+def dataset_generator(patient_id, scan_id, m_phases, f_phases, root_path, ct_path_dict, z_max):
     """
-    Generates tensors with CT data of moving images and fixed image.
-    Tensors have shape [len(m_phases),2,z,x,y]
-
+    Generates dataset object with pairs of CT data of moving images and fixed image.
     :param patient_id: [int] patient number
     :param scan_id: [string] ID of the scan
     :param m_phases: [array of ints] array with the phases to put in the array
-    :param f_phase: [int] fixed phase
-    :param phases: [array of ints] array with the phases to put in the array
+    :param f_phases: [int] fixed phase
     :param root_path: [string] root path to 4DCT folders.
     :param ct_path_dict: [dict] dictionary with all filepaths to the CT data.
-
-    :return stacked_tensor: torch.tensor shape [len(m_phases),2,z,x,y] tensor with CT data of all fixed and moving images.
+    :param z_max: [int] max z value for the CT volume. (Change this)
+    :return ct_dataset: return a dataset class with fixed and moving tensor pairs.
     """
 
-    moving_tensor = torch.tensor(CT_dataset(patient_id, scan_id, m_phases, root_path, ct_path_dict), dtype=torch.float)
-    # repeat fixed image data to match the moving image array shape.
-    fixed_tensor =  torch.tensor(np.repeat(CT_dataset(patient_id, scan_id, [f_phase], root_path, ct_path_dict), len(m_phases),
-                              axis=0), dtype=torch.float)
+    if len(m_phases) != len(f_phases):
+        raise Exception("Length of m_phases array does not match f_phases array.")
 
-    stacked_tensor = torch.stack([fixed_tensor,moving_tensor],1)
-    return stacked_tensor
+    moving_tensor = torch.tensor(CT_dataset(patient_id, scan_id, m_phases, root_path, ct_path_dict)[:, :z_max, ...],
+                                 dtype=torch.float)
+    fixed_tensor = torch.tensor(CT_dataset(patient_id, scan_id, f_phases, root_path, ct_path_dict)[:, :z_max, ...],
+                                dtype=torch.float)
+
+    # Add additional axis to tensor, and make a dataset object from ct_dataset class.
+    return ct_dataset(fixed_tensor[:, None, ...], moving_tensor[:, None, ...])
