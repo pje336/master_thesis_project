462ed93d78b11394575c19b62d5af5dd1bfaebb6
Thomas van der Meulen <t.vandermeulen-1@student.tudelft.nl>
2022-04-14 14:14:29 +0200

    delete old dataset generator file and rename the parallel dataset generator

--- dataset_generator.py
+++ dataset_generator.py
@@ -1,145 +1,213 @@
 import os
+from random import randint
 
 import numpy as np
 import pydicom
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 
 
-class ct_dataset(Dataset):
+class SampleDataset(Dataset):
+    """Dataset class with fixed and moving tensor pairs each of shape [1,z,x,y]. Inherited from torch.utils.data.Dataset
+        Generates a dataset class with fixed and moving tensor pairs.
+        The images can be augmented via a random shift in x and y direction by the shift attribute.
+
+
+       Attributes:
+            root_path: [string] Root path to 4DCT folders.
+            ct_path_dict: [dict] dictionary with all file paths to the dicom files..
+            scan_keys: [array]: array with keys for each scan. e.g: [[patient_id,scan_id,[f_phase,m_phase]],...]
+            dimensions: [1d array] array with dimensions to crop the image [z_min,z_max,x_min,x_max,y_min,y_max]
+            shift: [1d array] array with max up and down shift [x_down,x_up,y_down,y_up] (can be zeros)
     """
-    Dataset class with fixed and moving tensor pairs of shape [len(phases),1,z,x,y]
-    """
 
-    def __init__(self, fixed, moving):
-        self.fixed = fixed
-        self.moving = moving
+    def __init__(self, root_path, ct_path_dict, scan_keys, dimensions, shift):
+        self.root_path = root_path
+        self.ct_path_dict = ct_path_dict
+        self.scans_keys = scan_keys
+        self.dimensions = dimensions
+        self.shift = shift
 
     def __len__(self):
-        return len(self.fixed)
+        """Return the number of samples in the dataset."""
+        return len(self.scans_keys)
 
     def __getitem__(self, index):
-        _fixed = self.fixed[index]
-        _moving = self.moving[index]
+        """Gets the fixed and moving tensor by reading the dicom files and applying data augmention"""
+        patient_id, scan_id, phases = self.scans_keys[index]
+
+        # Get scan data.
+        _fixed = read_ct_data_file(self.root_path, self.ct_path_dict[patient_id][scan_id][phases[0]], self.dimensions)
+        _moving = read_ct_data_file(self.root_path, self.ct_path_dict[patient_id][scan_id][phases[1]], self.dimensions)
+
+        # Apply shift for data augmentation
+        # determine the random shift in x and y directions.
+        x_shift = randint(-self.shift[0], self.shift[1])
+        y_shift = randint(-self.shift[2], self.shift[3])
+
+        # This tuple is y,x because pad function is weird. See documentation of torch.nn.functional.pad
+        pad = (abs(y_shift), abs(y_shift), abs(x_shift), abs(x_shift))
+
+        # Apply padding, rolling and then cropping to get the shifted image.
+        # Note: The dims in roll are switched again, it is all super vague why this is.
+        _fixed = torch.nn.functional.pad(_fixed[0], pad).roll(shifts=(x_shift, y_shift), dims=(2, 1))[None, :,
+                 pad[2]:self.dimensions[3] + pad[2],
+                 pad[0]:self.dimensions[5] + pad[0]]
+        _moving = torch.nn.functional.pad(_moving[0], pad).roll(shifts=(x_shift, y_shift), dims=(2, 1))[None, :,
+                  pad[2]:self.dimensions[3] + pad[2],
+                  pad[0]:self.dimensions[5] + pad[0]]
 
         return _fixed, _moving
 
     def shape(self):
-        return self.fixed.shape
+        """ Return the shape of the fixed/moving tensor. """
+        return [self.dimensions[1] - self.dimensions[0], self.dimensions[3] - self.dimensions[2],
+                self.dimensions[5] - self.dimensions[4]]
 
 
-def read_CT_data_file(root_path, filepath):
+def generate_dataset(scan_keys, root_path, ct_path_dict, dimensions, shift, batch_size, shuffle=True):
+    """ Generates dataset objects with  batches pairs of fixed_tensor, moving_tensor based on scan_keys.
+    It generates a ct_dataset instance and then makes a dataloader object with the given batch_size.
+    The data pairs are read on the fly when iterating and therefore do not have high memory use.
+
+    First checks if all the scans files from scan_keys are present, otherwise an error is raised.
+
+
+
+    Args:
+        scan_keys: [array]: array with keys for each scan. e.g: [[patient_id,scan_id,[f_phase,m_phase]],...]
+        root_path: [string] Root path to 4DCT folders.
+        ct_path_dict: [dict] dictionary with all file paths to the dicom files..
+        dimensions: [1d array] array with dimensions to crop the image [z_min,z_max,x_min,x_max,y_min,y_max]
+        shift: [1d array] array with max up and down shift [x_down,x_up,y_down,y_up] (can be zeros)
+        batch_size: [int] number of samples in each batch
+        shuffle:[Boolean] shuffle the samples when generating batches (True by default)
+
+    Returns: A dataset from DataLoader-object which can be used to load batches with pairs of fixed_tensor, moving_tensor tensors.
+            One could iterate over the dataset as the following:
+            "for fixed_tensor, moving_tensor in  dataset: "
+            Shapes:
+            fixed_tensor: [Batch_size,1,z,x,y]
+            moving_tensor: [Batch_size,1,z,x,y]
+
+
+
+    Raises:
+        ValueError: If the root_path doesn't end with a "/"
+        ValueError: If the length of scan_keys is zero.
+        ValueError: If none of the dicom files from scan_keys can be read
+        ValueError: If some of the dicom files from scan_keys can be read
+
     """
-    Imports all CT data by reading all dcm files in the given directory and normalises the data.
-    :param root_path: [string] Root path to 4DCT folders.
-    :param filepath: [string] String with the specific folder which holds CT data.
-    :return: CT_data: [3d array] 3d numpy array of shape [z,x,y] with normalised CT data.
+
+    # Checking the root_path and the len of the scan_keys array.
+    if root_path[-1] != "/":
+        raise ValueError("root_path should end with '/'")
+    if len(scan_keys) == 0:
+        raise ValueError("Scan_keys has no objects.")
+
+    # Checking if the files for scan_keys excist. Else, raise an ValueError
+    keys_not_found = scan_key_checker(scan_keys, root_path, ct_path_dict)
+    print(len(keys_not_found), len(scan_keys))
+    if len(keys_not_found) == len(scan_keys):
+        raise ValueError("None of the dicom"
+                         " files found, is the root path correct?")
+    elif len(keys_not_found) != 0:
+        raise ValueError("The dicom files for the following keys where not found:", keys_not_found)
+
+    data = SampleDataset(root_path, ct_path_dict, scan_keys, dimensions, shift)
+    return DataLoader(data, batch_size, shuffle)
+
+
+def read_ct_data_file(root_path, filepath, dimensions):
+    """Imports  CT data by reading all dicom files in the given directory and normalises the data.
+
+
+    Args:
+        root_path: [string] Root path to 4DCT folders.
+        filepath: [string] String with the specific folder whichdat  holds CT data.
+        dimensions: [1d array] array with dimensions to crop the image [z_min,z_max,x_min,x_max,y_min,y_max]
+
+    Returns:
+        ct_data_tensor: [4d tensor] 4d pytorch tensor of shape [1,z,x,y] with normalised CT data.
+
     """
     # Get all files and paths.
     full_path, dirs, files = next(os.walk(root_path + filepath + "/"))
-    # read a single file to get the dimensions.
     data = pydicom.dcmread(full_path + files[0])
 
     # Setup a empty array with correct shape (z,x,y)
-    CT_data = np.zeros((int(data.ImagesInAcquisition), int(data.Rows), int(data.Columns)))
+    ct_data = np.zeros((int(data.ImagesInAcquisition), int(data.Rows), int(data.Columns)))
 
     # Iterate over files axis.
     for z, file in enumerate(files):
-        CT_data[z, :, :] = pydicom.dcmread(full_path + file).pixel_array
+        ct_data[z, :, :] = pydicom.dcmread(full_path + file).pixel_array
 
     # normalise data
-    CT_data /= np.max(CT_data)
-    return CT_data
+    ct_data_cropped = ct_data[dimensions[0]:dimensions[1], dimensions[2]:dimensions[3], dimensions[4]:dimensions[5]]
+    ct_data_cropped /= np.max(ct_data_cropped)
+    ct_data_tensor = torch.tensor(ct_data_cropped, dtype=torch.float)
+
+    return ct_data_tensor[None, ...]
 
 
-def CT_dataset(patient_id, scan_id, phases, root_path, ct_path_dict):
+def scan_key_generator(dictionary, patient_ids=None, scan_list=None):
+    """Generates an array with arrays containing keys for the dictionary.
+    One could get the keys for all scans in the dictionary or for a specific patient or a specific scan of a patient.
+
+    Args:
+        dictionary: [dict] dictionary with all file paths to the dicom files..
+        patient_ids: array with patients (If None, then all patients)
+        scan_list: array with specific scan of patient (If None, then all scans of patient)
+
+    Returns:
+            scan_keys: [array]: array with arrays of keys for each scan. e.g: [[patient_id,scan_id,[f_phase,m_phase]],...]
+
     """
-    Generates a dataset of 4d array of CT data for a specific patient_id and scan_id for given phases.
-    It return a 4d array with shape [len(phases),z,x,y].
-    :param patient_id: [int] patient number
-    :param scan_id: [string] ID of the scan
-    :param phases: [array of ints] array with the phases to put in the array
-    :param root_path: [string] root path to 4DCT folders.
-    :param ct_path_dict: [dict] dictionary with all filepaths to the CT data.
-    :return: ct_data: array[len(phases),z,x,y] array with CT data for all the phases.
-    """
-    for i, phase in enumerate(phases):
+    scan_keys = []
+    if patient_ids is None:
+        patient_ids = dictionary.keys()
 
-        try:  # try to retrieve the filepath from dict.
-            ct_filepath = ct_path_dict[str(patient_id)][scan_id][str(phase)]
-        except KeyError:
-            print("!Error: Key not found in the filepath dictionary.")
-            print("key: [{}][{}][{}]".format(str(patient_id), scan_id, str(phase)))
-            return
-
-        if i == 0:  # make the initial array on the first run.
-            ct_data = np.zeros(((len(phases),) + np.shape(read_CT_data_file(root_path, ct_filepath))))
-
-        ct_data[i] = read_CT_data_file(root_path, ct_filepath)
-
-    return ct_data
-
-
-def dataset_generator(patient_id, scan_id, m_phases, f_phases, root_path, ct_path_dict, relative_size, pad):
-    """
-    Generates dataset object with pairs of CT data of moving images and fixed image.
-    Applies padding or removes some slices from the z-axis
-    :param patient_id: [int] patient number
-    :param scan_id: [string] ID of the scan
-    :param m_phases: [array of ints] array with the phases to put in the array
-    :param f_phases: [int] fixed phase
-    :param root_path: [string] root path to 4DCT folders.
-    :param ct_path_dict: [dict] dictionary with all filepaths to the CT data.
-    :param relative_size: [int] max z value for the CT volume. (Change this)
-    :param pad: [boolean] Add padding instead of reducing the size.
-    :return ct_dataset: return a dataset class with fixed and moving tensor pairs.
-    """
-
-    if len(m_phases) != len(f_phases):
-        raise Exception("Length of m_phases array does not match f_phases array.")
-
-    moving_tensor = torch.tensor(CT_dataset(patient_id, scan_id, m_phases, root_path, ct_path_dict),
-                                 dtype=torch.float)
-    fixed_tensor = torch.tensor(CT_dataset(patient_id, scan_id, f_phases, root_path, ct_path_dict),
-                                dtype=torch.float)
-
-    # Adjust the size of the z-axis.
-    if fixed_tensor.shape[1] % (2 ** relative_size) == 0:
-        # if the z-axis length is a multiple of the relative_size
-        # return it as a dataset.
-        return ct_dataset(fixed_tensor[:, None, ...], moving_tensor[:, None, ...])
-
-    elif fixed_tensor.shape[1] < (2 ** relative_size) or pad:
-        # if the z-axis length is smaller than the relative volume
-        # or when 'pad' is true. apply padding.
-        print("Applying padding for  the Z-axis")
-
-        if fixed_tensor.shape[1] < (2 ** relative_size):  # if the z-axis is smaller than relative volume
-            under_size = (2 ** relative_size) - fixed_tensor.shape[1]
+    for patient_id in patient_ids:
+        if scan_list is None:
+            scan_ids = dictionary[patient_id].keys()
         else:
-            new_size = (fixed_tensor.shape[1] // (2 ** relative_size) + 1) * (2 ** relative_size)
-            under_size = new_size - fixed_tensor.shape[1]
+            scan_ids = scan_list
 
-        if under_size % 2 != 0:  # for odd under_size
-            under_size += 1
-            odd = True
-        else:
-            odd = False
+        for scan_id in scan_ids:
 
-        # Apply padding
-        moving_tensor = torch.nn.functional.pad(moving_tensor, (0, 0, 0, 0, under_size // 2, under_size // 2))
-        fixed_tensor = torch.nn.functional.pad(fixed_tensor, (0, 0, 0, 0, under_size // 2, under_size // 2))
+            for m_phase in dictionary[patient_id][scan_id].keys():
 
-        if odd:  # for odd under_size, remove the last slice of zeros
-            return ct_dataset(fixed_tensor[:, None, :-1, ...],
-                              moving_tensor[:, None, :-1, ...])
-        else:
-            return ct_dataset(fixed_tensor[:, None, ...],
-                              moving_tensor[:, None, ...])
+                for f_phase in dictionary[patient_id][scan_id].keys():
+                    scan_keys.append([patient_id, scan_id, [f_phase, m_phase]])
+    return scan_keys
 
-    else:
-        print("reducing the size in the Z-axis")
-        oversize = fixed_tensor.shape[1] % (2 ** relative_size)
-        # Add additional axis to tensor, reduce the z-axis  and make a dataset object from ct_dataset class.
-        return ct_dataset(fixed_tensor[:, None, oversize // 2:-(oversize - oversize // 2), ...],
-                          moving_tensor[:, None, oversize // 2:-(oversize - oversize // 2), ...])
+
+def scan_key_checker(scan_keys, root_path, dictionary):
+    """Checks if the dicom files for each scan key exists.
+    If it does not it will be added to keys_not_found not found array.
+
+
+    Args:
+        scan_keys: [array]: array with keys for each scan. e.g: [[patient_id,scan_id,[f_phase,m_phase]],...]
+        root_path: [string] Root path to 4DCT folders.
+        dictionary: [dict] dictionary with all file paths to the dicom files.*.
+
+    Returns:
+        keys_not_found: Array with scan keys of which the dicom files where not found.
+
+    """
+    keys_not_found = []
+
+    for i, [patient, scan, phases] in enumerate(scan_keys):
+
+        for phase in phases:
+            filepath = dictionary[patient][scan][phase]
+
+            try:
+                next(os.walk(root_path + filepath + "/"))
+            except:
+                keys_not_found.append(scan_keys[i])
+                break  # we do not need to check the other phase anymore
+
+    return keys_not_found
