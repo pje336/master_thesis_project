c69c3e684d7300bade56cc7bfd11cec575c6dec9
Thomas van der Meulen <t.vandermeulen-1@student.tudelft.nl>
2022-04-22 09:34:28 +0200

    Load  trained model weights and evaluation the model on a evuluation_dataset and calculate jacobian

--- prediction.py
+++ prediction.py
@@ -1,3 +1,11 @@
+import numpy as np
+import torch
+
+import voxelmorph
+from ct_path_dict import ct_path_dict
+from dataset_generator import scan_key_generator, generate_dataset
+
+
 def read_model_parameters_from_file(model_path, filename="training_parameters.txt"):
     """
     Read the model parameters from the file training_parameters.txt and convert the values into variables.
@@ -14,22 +22,43 @@ def read_model_parameters_from_file(model_path, filename="training_parameters.tx
     return learning_rate, epochs, batch_size, loss_weights, patient_id, scan_id_training, scan_id_validation, \
            validation_batches, nb_features, data_shape
 
+# Filepaths for the CT data and the trained model.
+root_path_data = "C:/Users/pje33/Google Drive/Sync/TU_Delft/MEP/4D_lung_CT/4D-Lung-256/"
+trained_model_path = "C:/Users/pje33/Google Drive/Sync/TU_Delft/MEP/saved_models/training_2022_04_20_10_56_34/"
+trained_model_dict_name = "voxelmorph_model_epoch_0.pth"
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-model_path = "C:/Users/pje33/Google Drive/Sync/TU_Delft/MEP/saved_models/training_2022_04_20_10_56_34/"
-
+# Obtain training parameters.
 learning_rate, epochs, batch_size, loss_weights, patient_id, scan_id_training, scan_id_validation, validation_batches, \
-nb_features, data_shape = read_model_parameters_from_file(model_path)
-#
-# x_data = torch.tensor(CT_data_0[np.newaxis,np.newaxis,  ...], dtype=torch.float)
-# y_data = torch.tensor(CT_data_90[np.newaxis,np.newaxis, ...], dtype=torch.float)
-#
-# prediction, pos_flow = model(x_data, y_data)
-#
-# predicted_CT = prediction[0,0,...].detach().numpy()
-# np.save("predicted_CT.npy",prediction[0,0,...].detach().numpy())
-#
-# predicted_CT_epoch_1 = np.load("predicted_CT_epoch_1.npy")
-# predicted_CT_epoch_2 = np.load("predicted_CT_epoch_2.npy")
-# predicted_CT_epoch_3 = np.load("predicted_CT_epoch_3.npy")
-#
-# slice_viewer([CT_data_90,predicted_CT_epoch_1,predicted_CT_epoch_2,predicted_CT_epoch_3])
+nb_features, data_shape = read_model_parameters_from_file(trained_model_path)
+
+# Make the model and load the weights.
+model = voxelmorph.networks.VxmDense(data_shape, nb_features, int_downsize=1, bidir=True)
+model.load_state_dict(torch.load(trained_model_path + trained_model_dict_name))
+model.to(device)
+model.eval()
+
+# Parameters for evaluation dataset
+patient_id_evaluation = ["107"]
+scan_id_evaluation = ["06-02-1999-p4-89680"]
+batch_size = 1
+dimensions = [0, data_shape[0], 0, data_shape[1], 0, data_shape[2]]
+shift = [0, 0, 0, 0]
+
+# Make an evaluation dataset.
+evaluation_scan_keys = scan_key_generator(ct_path_dict, patient_id_evaluation, scan_id_evaluation)
+evaluation_set = generate_dataset(evaluation_scan_keys, root_path_data, ct_path_dict, dimensions, shift, batch_size)
+
+# Run through all the samples in the evaluation_set.
+for moving_tensor, fixed_tensor in evaluation_set:
+    moving_tensor = moving_tensor.to(device)
+    fixed_tensor = fixed_tensor.to(device)
+    prediction = model(moving_tensor, fixed_tensor)
+
+    # Calculate jacobian for every voxel in deformation map.
+    jac = voxelmorph.py.utils.jacobian_determinant(prediction[-1][0].permute(1, 2, 3, 0).cpu().detach().numpy())
+    print("percentage of voxel with negative jacobian:", np.size(jac[jac < 0]) / np.size(jac) * 100)
+
+    del prediction
+    del fixed_tensor
+    del moving_tensor
