2e2f1c3404fffb20d27bc64fcbc56647517c4531
Thomas van der Meulen <t.vandermeulen-1@student.tudelft.nl>
2022-04-29 15:32:41 +0200

    add code to calculate the deformation of contours for the scans

--- prediction.py
+++ prediction.py
@@ -1,9 +1,10 @@
-import numpy as np
 import torch
 
 import voxelmorph
-from CT_path_dict.ct_path_dict import ct_path_dict
+from CT_path_dict.ct_path_dict import ct_path_dict, contour_dict
+from contours.contour import *
 from dataset_generator import scan_key_generator, generate_dataset
+from voxelmorph.torch.layers import SpatialTransformer
 
 
 def read_model_parameters_from_file(model_path: str, filename: str = "training_parameters.txt"):
@@ -23,10 +24,50 @@ def read_model_parameters_from_file(model_path: str, filename: str = "training_p
            validation_batches, nb_features, data_shape, int_downsize
 
 
+def deform_contour(flow_field, scan_key, root_path, ct_path_dict, contour_dict, z_shape):
+    """
+    Function to perform deformation of contours for a specific scan using the flowfield of that scan.
+    Args:
+        flow_field:
+        scan_key: [array]: array with key for  scan. e.g: [[(patient_id), (scan_id), (f_phase, m_phase)]]
+        root_path: [string] Root path to 4DCT folders.
+        ct_path_dict: [dict] dictionary with all file paths to the scan dicom files..
+        contour_dict: [dict] dictionary with all file paths to the contour dicom file.
+        z_shape: [array] min and max z-value [z_min,z_max]
+
+    Returns: 4-d array with warped_contour [len(contours),z,x,y]
+
+    """
+    # Obtain scan information and the corresponding file paths.
+    [[(patient_id), (scan_id), (f_phase, m_phase)]] = scan_key
+    path_images = root_path + ct_path_dict[patient_id[0]][scan_id[0]][m_phase[0]]
+    path_contour = root_path + contour_dict[patient_id[0]][scan_id[0]][m_phase[0]]
+
+
+    contour_data = dicom.read_file(path_contour + '/1-1.dcm')
+    # iterate over all the contours/
+    warped_contour = torch.zeros((len(get_roi_names(contour_data)),80,512,512))
+    for index in range(len(get_roi_names(contour_data))):
+        # Get the contour volume and convert to float tensor..
+        mask = get_mask(path_images, path_contour, index)[z_shape[0]:z_shape[1]]
+        mask_tensor = torch.tensor(np.float64(mask[None, None, ...]), dtype=torch.float)
+        scale_factor = tuple(np.array(mask_tensor.shape)[-3:] / np.array(flow_field.shape)[-3:])
+        # upsample the flowfield to the correct size.
+        flow_field_upsampled = torch.nn.functional.interpolate(flow_field, scale_factor= scale_factor,
+                                                               mode='trilinear', align_corners=True)
+
+        # apply transformation to get warped_contour
+        transformer = SpatialTransformer(np.shape(mask))
+        warped_contour[index] = transformer(mask_tensor, flow_field_upsampled)
+
+    return warped_contour
+
+
 # Filepaths for the CT data and the trained model.
 root_path_data = "C:/Users/pje33/Google Drive/Sync/TU_Delft/MEP/4D_lung_CT/4D-Lung-256/"
-trained_model_path = "C:/Users/pje33/Google Drive/Sync/TU_Delft/MEP/saved_models/training_2022_04_22_08_53_18/"
-trained_model_dict_name = "voxelmorph_model_epoch_3.pth"
+root_path_contour = "C:/Users/pje33/Google Drive/Sync/TU_Delft/MEP/4D_lung_CT/4D-Lung-512/"
+trained_model_path = "C:/Users/pje33/Google Drive/Sync/TU_Delft/MEP/saved_models/training_2022_04_25_12_33_59/"
+trained_model_dict_name = "voxelmorph_model_epoch_9.pth"
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # Obtain training parameters.
@@ -44,18 +85,23 @@ print("Model imported")
 patient_id_evaluation = ["107"]
 scan_id_evaluation = ["06-02-1999-p4-89680"]
 batch_size = 1
-dimensions = [0, data_shape[0], 0, data_shape[1], 0, data_shape[2]]
+dimensions = [0, 80, 0, 256, 0, 256]
 shift = [0, 0, 0, 0]
+
 
 # Make an evaluation dataset.
 evaluation_scan_keys = scan_key_generator(ct_path_dict, patient_id_evaluation, scan_id_evaluation)
 evaluation_set = generate_dataset(evaluation_scan_keys, root_path_data, ct_path_dict, dimensions, shift, batch_size)
-
 # Run through all the samples in the evaluation_set.
-for moving_tensor, fixed_tensor in evaluation_set:
+for moving_tensor, fixed_tensor, scan_key in evaluation_set:
+    print(scan_key)
+
     moving_tensor = moving_tensor.to(device)
     fixed_tensor = fixed_tensor.to(device)
     prediction = model(moving_tensor, fixed_tensor)
+
+    # compute the deformation of the contours.
+    deform_contour(prediction[-1], scan_key, root_path_contour, ct_path_dict, contour_dict, dimensions[:2])
 
     # Calculate jacobian for every voxel in deformation map.
     jac = voxelmorph.py.utils.jacobian_determinant(prediction[-1][0].permute(1, 2, 3, 0).cpu().detach().numpy())
