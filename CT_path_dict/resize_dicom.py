90cf7aaf04986842c983b8a7df9b4ebd4579806b
Thomas van der Meulen <t.vandermeulen-1@student.tudelft.nl>
2022-05-03 15:10:45 +0200

    change resize method of dicom files

--- CT_path_dict/resize_dicom.py
+++ CT_path_dict/resize_dicom.py
@@ -7,15 +7,15 @@ import os
 
 import numpy as np
 import pydicom
-from skimage.transform import resize
+from skimage.transform import resize,downscale_local_mean
 
 from CT_path_dict.ct_path_dict import ct_path_dict
+import matplotlib.pyplot as plt
 
-dimensions_resize = [128, 128]
+dimensions_resize = [256, 256]
 
 root_path = "C:/Users/pje33/Google Drive/Sync/TU_Delft/MEP/4D_lung_CT/4D-Lung-512/"
-root_path_resize = "C:/Users/pje33/Google Drive/Sync/TU_Delft/MEP/4D_lung_CT/4D-Lung-{}/".format(
-    str(dimensions_resize[0]))
+root_path_resize = "C:/Users/pje33/Google Drive/Sync/TU_Delft/MEP/4D_lung_CT/4D-Lung-{}/".format(str(dimensions_resize[0]))
 
 for patient_id in ct_path_dict.keys():
     for scan_id in ct_path_dict[patient_id].keys():
@@ -36,11 +36,9 @@ for patient_id in ct_path_dict.keys():
             for z, file in enumerate(files):
                 # open file and get pixel data.
                 data_original = pydicom.dcmread(full_path + file)
-                data_pixels = data_original.pixel_array
+                data_pixels = np.array(data_original.pixel_array, np.float)
                 # resize the pixeldata and scale it to 16bit int.
-                data_resized = np.array(65534 * resize(data_pixels, dimensions_resize, anti_aliasing=True),
-                                        dtype=np.uint16)
-
+                data_resized = np.array(downscale_local_mean(data_pixels,(2,2)),dtype=np.uint16)
                 # Change the pixeldata to the resized one and update the shape and save it.
                 data_original.PixelData = data_resized.tobytes()
                 data_original.Rows, data_original.Columns = np.shape(data_resized)
