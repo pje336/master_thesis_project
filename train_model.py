f15ab8f7d6e61d2fd6a22372d39094fd58d8a6ac
Thomas van der Meulen <t.vandermeulen-1@student.tudelft.nl>
2022-05-10 11:31:59 +0200

    undo commenting of code in training file

--- train_model.py
+++ train_model.py
@@ -47,75 +47,74 @@ def train_model(vxm_model, train_dataset, validation_dataset, epochs, learning_r
         for fixed_tensor, moving_tensor, _ in train_dataset:
             batch += 1
 
-            
             if torch.cuda.is_available():
                 device = torch.device("cuda:0")
                 moving_tensor = moving_tensor.to(device)
                 fixed_tensor = fixed_tensor.to(device)
-            #
-            # # Obtain next image pare from iterator.
-            # prediction = vxm_model(moving_tensor, fixed_tensor)
-            #
-            # # Calculate loss for all the loss functions.
-            # loss_total = 0
-            # loss_array = []
-            # for j, loss_function in enumerate(losses):
-            #     loss = loss_function(fixed_tensor, prediction[j]) * loss_weights[j]
-            #     loss_array.append(float(loss))
-            #     loss_total += loss
-            # print("epoch {} of {}, Batch: {} - Loss: {}".format(epoch + 1, epochs, batch, loss_array))
-            #
-            # epoch_loss += float(loss_total)
-            #
-            # # Remove variables to create more space in ram.
-            # del moving_tensor
-            # del prediction
-            # torch.cuda.empty_cache()
-            #
-            # # Apply back propagation
-            # optimizer.zero_grad()
-            # loss_total.backward()
-            # optimizer.step()
-            #
-            # del loss_total
-            #
-            # if torch.cuda.is_available():
-            #     torch.cuda.empty_cache()
-            #
-            # if batch == steps_per_epoch:
-            #     epoch_training_loss.append(epoch_loss / batch)
-            #
-            #     # Here we will validate the model with data from the validation_dataset.
-            #     with torch.no_grad():
-            #         validation_batches = 0
-            #         validation_loss = 0
-            #         for fixed_tensor, moving_tensor, _ in validation_dataset:
-            #             if torch.cuda.is_available():
-            #                 device = torch.device("cuda:0")
-            #                 moving_tensor = moving_tensor.to(device)
-            #                 fixed_tensor = fixed_tensor.to(device)
-            #             validation_batches += 1
-            #             loss_total = 0
-            #             prediction = vxm_model(moving_tensor, fixed_tensor)
-            #
-            #             # Calculate loss for all the loss functions.
-            #             for j, loss_function in enumerate(losses):
-            #                 loss_total += loss_function(fixed_tensor, prediction[j]) * loss_weights[j]
-            #             validation_loss += float(loss_total)
-            #
-            #         epoch_validation_loss.append(validation_loss / validation_batches)
-            #         print("validation loss: {}".format(epoch_validation_loss[-1]))
-            #
-            #     # Save the model
-            #     torch.save(vxm_model.state_dict(), saving_file_path + "voxelmorph_model_epoch_{}.pth".format(epoch))
-            #     torch.save(optimizer.state_dict(), saving_file_path + "optimiser_epoch_{}.pth".format(epoch))
-            #     write_string_to_file(saving_file_path, "training_loss.txt", str(epoch_training_loss))
-            #     write_string_to_file(saving_file_path, "validation_loss.txt", str(epoch_validation_loss))
-            #     vxm_model.train()
-            #
-            #     batch = 0
-            #     epoch_loss = 0
-            #     epoch += 1
+
+            # Obtain next image pare from iterator.
+            prediction = vxm_model(moving_tensor, fixed_tensor)
+
+            # Calculate loss for all the loss functions.
+            loss_total = 0
+            loss_array = []
+            for j, loss_function in enumerate(losses):
+                loss = loss_function(fixed_tensor, prediction[j]) * loss_weights[j]
+                loss_array.append(float(loss))
+                loss_total += loss
+            print("epoch {} of {}, Batch: {} - Loss: {}".format(epoch + 1, epochs, batch, loss_array))
+
+            epoch_loss += float(loss_total)
+
+            # Remove variables to create more space in ram.
+            del moving_tensor
+            del prediction
+            torch.cuda.empty_cache()
+
+            # Apply back propagation
+            optimizer.zero_grad()
+            loss_total.backward()
+            optimizer.step()
+
+            del loss_total
+
+            if torch.cuda.is_available():
+                torch.cuda.empty_cache()
+
+            if batch == steps_per_epoch:
+                epoch_training_loss.append(epoch_loss / batch)
+
+                # Here we will validate the model with data from the validation_dataset.
+                with torch.no_grad():
+                    validation_batches = 0
+                    validation_loss = 0
+                    for fixed_tensor, moving_tensor, _ in validation_dataset:
+                        if torch.cuda.is_available():
+                            device = torch.device("cuda:0")
+                            moving_tensor = moving_tensor.to(device)
+                            fixed_tensor = fixed_tensor.to(device)
+                        validation_batches += 1
+                        loss_total = 0
+                        prediction = vxm_model(moving_tensor, fixed_tensor)
+
+                        # Calculate loss for all the loss functions.
+                        for j, loss_function in enumerate(losses):
+                            loss_total += loss_function(fixed_tensor, prediction[j]) * loss_weights[j]
+                        validation_loss += float(loss_total)
+
+                    epoch_validation_loss.append(validation_loss / validation_batches)
+                    print("validation loss: {}".format(epoch_validation_loss[-1]))
+
+                # Save the model
+                torch.save(vxm_model.state_dict(), saving_file_path + "voxelmorph_model_epoch_{}.pth".format(epoch))
+                torch.save(optimizer.state_dict(), saving_file_path + "optimiser_epoch_{}.pth".format(epoch))
+                write_string_to_file(saving_file_path, "training_loss.txt", str(epoch_training_loss))
+                write_string_to_file(saving_file_path, "validation_loss.txt", str(epoch_validation_loss))
+                vxm_model.train()
+
+                batch = 0
+                epoch_loss = 0
+                epoch += 1
 
     if torch.cuda.is_available():
         torch.cuda.empty_cache()
