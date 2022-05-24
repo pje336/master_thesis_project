94d0b6d7327990c1ee27c4d405f6d8624dce6c91
Thomas van der Meulen <t.vandermeulen-1@student.tudelft.nl>
2022-05-24 14:55:49 +0200

    Update the sliceview to have labels, collorbars and multiple row/colums

--- slice_viewer.py
+++ slice_viewer.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 class IndexTracker:
     """
@@ -10,9 +10,16 @@ class IndexTracker:
     :param Xs: array of data-arrays in shape [z,x,y]
     """
 
-    def __init__(self, axes, Xs):
+    def __init__(self, axes, Xs, titles, fig):
+
         self.slices, rows, cols = Xs[0].shape
         self.ind = self.slices // 2
+        self.titles = titles
+        self.fig = fig
+
+        self.min = np.min([np.min(x) for x in Xs ])
+        self.max = np.max([np.max(x) for x in Xs])
+        print(self.min)
 
         # check how many axes there are.
         try:
@@ -25,7 +32,8 @@ class IndexTracker:
         for i, ax in enumerate(axes):
             exec("self.ax{} = ax".format(i))
             exec("self.X{} = Xs[{}]".format(i, i))
-            exec("self.im{} = self.ax{}.imshow(self.X{}[self.ind,:, :])".format(i, i, i))
+            exec("self.im{} = self.ax{}.imshow(self.X{}[self.ind,:, :], vmin = self.min, vmax = self.max, cmap='viridis')".format(i, i, i))
+            exec("fig.colorbar(self.im{},ax = ax)".format(i))
             exec("self.im{} = self.ax{}.set_xlabel('x')".format(i, i))
         self.update()
 
@@ -39,17 +47,27 @@ class IndexTracker:
 
     def update(self):
         for i in range(self.length):
-            exec("self.im{} = self.ax{}.imshow(self.X{}[self.ind, :, :])".format(i, i, i))
+            exec("self.im{} = self.ax{}.imshow(self.X{}[self.ind, :, :], vmin = self.min, vmax = self.max, cmap='viridis')".format(i, i, i))
+
             exec("self.ax{}.set_ylabel('slice %s  y' % self.ind)".format(i))
+            exec("self.ax{}.set_title(self.titles[{}])".format(i,i))
             exec("self.im{}.axes.figure.canvas.draw()".format(i))
 
 
-def slice_viewer(CT_data):
+def slice_viewer(CT_data, titles, shape = None):
     """
     function to view slices of CT data using plt. You can scroll through the slices.
     :param CT_data: array of data-arrays in shape [z,x,y]
     """
-    fig, axes = plt.subplots(1, len(CT_data))
-    tracker = IndexTracker(axes, CT_data)
+
+    if shape is None:
+
+        fig, axes = plt.subplots(1,len(CT_data))
+        tracker = IndexTracker(axes, CT_data, titles, fig)
+    else:
+        fig, axes = plt.subplots(shape[0], shape[1])
+        tracker = IndexTracker(axes.flatten()[:len(CT_data)], CT_data, titles, fig)
+
     fig.canvas.mpl_connect('scroll_event', tracker.on_scroll)
+    fig.tight_layout()
     plt.show()
